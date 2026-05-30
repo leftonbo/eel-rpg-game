be47e64030e5bdfee50338f4bf367823991202b1
LefTonbo <5452354+leftonbo@users.noreply.github.com>
2026-05-31 02:33:14 +0900

    🐛 翻訳漏れがあるでござる

--- src/game/i18n/resources/en/playerSkills.ts
+++ src/game/i18n/resources/en/playerSkills.ts
@@ -1,6 +1,8 @@
 const playerSkills = {
-        activeTitle: 'Active Skills',
-        passiveTitle: 'Passive Skills'
-    };
+    activeTitle: 'Active Skills',
+    passiveTitle: 'Passive Skills',
+    noActiveSkills: 'No active skills available',
+    noPassiveSkills: 'No passive skills available'
+};
 
 export default playerSkills;
